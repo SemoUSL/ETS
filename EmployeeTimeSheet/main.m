e07ae626529371437fb6742490cf949e83beeba5
Ismail <unbounded@Apples-MacBook.local>
2014-03-30 15:20:45 +0100

    Initial Commit

---
+++ EmployeeTimeSheet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EmployeeTimeSheet
+//
+//  Created by Ismail on 30/03/2014.
+//  Copyright (c) 2014 Unbounded Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ETSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ETSAppDelegate class]));
+    }
+}
